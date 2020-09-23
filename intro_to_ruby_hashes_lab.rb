e949df5aa028f4102181fb21f340e9a9acecac44
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-23 08:15:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,9 @@
 def base_hash
-  
+  my_hash {
+    :railroads => {
+      
+    }
+  }
 
 end
 
@@ -8,6 +12,17 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  my_hash {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        
+      },
+      :names => {
+        
+      }
+    }
+  }
 
 end
 
@@ -16,6 +31,35 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  my_hash {
+    
+    :railroads => {
+      
+      :pieces => 4,
+      
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200,
+      },
+      
+      :names => {
+        :reading_railroad => {
+          
+        },
+        :pennsylvania_railroad => {
+          
+        },
+        :b_and_o_railroad => {
+          
+        },
+        :shortline_railroad => {
+          
+        }
+      }
+    }
+  }
 
 end
 
